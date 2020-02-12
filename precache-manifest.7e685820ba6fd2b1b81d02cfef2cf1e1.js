15a1674f34182c18faadac04fa62bce365e0fc9a
Kush Gupta <reachkush.gupta@gmail.com>
2020-02-12 07:34:23 +0530

    Updates

---
+++ precache-manifest.7e685820ba6fd2b1b81d02cfef2cf1e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d0331e11d4a8de7620c8161d1e379c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e63d396b941b03c44d8d",
+    "url": "/todo/static/css/2.37afb15d.chunk.css"
+  },
+  {
+    "revision": "c190010e6faa1bf482b7",
+    "url": "/todo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e63d396b941b03c44d8d",
+    "url": "/todo/static/js/2.5aa4d6ce.chunk.js"
+  },
+  {
+    "revision": "c190010e6faa1bf482b7",
+    "url": "/todo/static/js/main.6b928ebd.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/todo/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/todo/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/todo/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/todo/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/todo/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/todo/static/media/icons-20.ca7934b3.eot"
+  }
+]);
